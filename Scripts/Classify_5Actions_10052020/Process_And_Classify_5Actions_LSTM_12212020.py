96de3dcfc625a4e12dbdf233c7d86c1a8ad52120
niharnsheth <nsheth7@uic.edu>
2020-12-22 14:34:45 -0600

    [UPDATE] Updated script to create features and one-hot vector labels. Added normalization functions.

--- Scripts/Classify_5Actions_10052020/Process_And_Classify_5Actions_LSTM_12212020.py
+++ Scripts/Classify_5Actions_10052020/Process_And_Classify_5Actions_LSTM_12212020.py
@@ -23,6 +23,10 @@ from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Dense
 
+from numpy import argmax
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
 #%% ============================================== Import Data ===================================================== %%#
 # --------------------------------------- Import data from separate folders -------------------------------------------#
 
@@ -40,10 +44,11 @@ data_folder_example = '3 Actions_09022020/Dilator/'
 save_data_folder = '5 Actions_10032020/Saved/12152020_1/'
 # Data array
 df = []
+labels = []
 
 for i in range(len(data_folder_list)):
     # get list of samples for action
-    action_no = i
+    #action_no = i
     csv_list_1 = [f for f in os.listdir(source_path + data_folder_list[i]) if fnmatch.fnmatch(f, '*.csv')]
     # read and import csv files
     for file in csv_list_1:
@@ -51,12 +56,26 @@ for i in range(len(data_folder_list)):
         # read data from csv into a dataframe
         file_df = pd.read_csv(source_path + data_folder_list[i] + file)
         df.append(file_df.drop(columns=['SId'], axis=1))
+        labels.append(i)
 
 
-#%% ============================================== Add Features ==================================================== %%#
-# ---------------------------------- Add linear and angular velocities to input data ----------------------------------#
+#%% =================================== Extract input features and output labels =================================== %%#
+# ------------------------ Add features: linear and angular velocities to input data ----------------------------------#
+# -------------------------------------------- Create one-hot vector labels ------------------------------------------ #
 
-columns_to_add = ['Vx', 'Vy', 'Vz', 'Va', 'Vb', 'Vg']
+# Convert euler to quaternions
+def euler_to_quaternion(roll, pitch, yaw):
+    c1 = np.cos(roll/2)
+    c2 = np.cos(pitch/2)
+    c3 = np.cos(yaw/2)
+    s1 = np.sin(roll/2)
+    s2 = np.sin(pitch/2)
+    s3 = np.sin(yaw/2)
+    quat_w = c1*c2*c3 - s1*s2*s3
+    quat_x = s1*s2*c3 + c1*c2*s3
+    quat_y = s1*c2*c3 + c1*s2*s3
+    quat_z = c1*s2*c3 - s1*c2*s3
+    return [quat_w, quat_x, quat_y, quat_z]
 
 
 # Calculates linear and angular velocity for given values
@@ -82,22 +101,7 @@ def cal_vel_for_range(pos_ori_values, time_values):
             val += 1
         return velocity
 
-# Convert euler to quaternions
-def euler_to_quaternion(roll, pitch, yaw):
-    c1 = np.cos(roll/2)
-    c2 = np.cos(pitch/2)
-    c3 = np.cos(yaw/2)
-    s1 = np.sin(roll/2)
-    s2 = np.sin(pitch/2)
-    s3 = np.sin(yaw/2)
-    quat_w = c1*c2*c3 - s1*s2*s3
-    quat_x = s1*s2*c3 + c1*c2*s3
-    quat_y = s1*c2*c3 + c1*s2*s3
-    quat_z = c1*s2*c3 - s1*c2*s3
-    return [quat_w, quat_x, quat_y, quat_z]
 
-
-#for file in df:
 for file_no in range(len(df)):
     # calculate absolute linear velocities
     pos_values_array = df[file_no][['X', 'Y', 'Z', 'PT']].to_numpy()
@@ -118,10 +122,95 @@ for file_no in range(len(df)):
     ang_velocity_quat_arr = qt.quaternion_time_series.angular_velocity(quat_arr, euler_angles_arr[:,-1])
     ang_velocity_quat_arr = np.absolute(ang_velocity_quat_arr)
 
-    partial_features = np.hstack((pos_values_array[:,0:2],np_quaternions_arr))
+    partial_features = np.hstack((pos_values_array[:,0:3],np_quaternions_arr))
     partial_features = np.hstack((partial_features,np.c_[x_linear_velocity],
                                   np.c_[y_linear_velocity],
                                   np.c_[z_linear_velocity]))
     partial_features = np.hstack((partial_features,ang_velocity_quat_arr))
 
     df[file_no] = partial_features
+
+# Creating one-hot vector matrix
+# integer encode
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(labels)
+#print(integer_encoded)
+# binary encode
+onehot_encoder = OneHotEncoder(sparse=False)
+integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+print(onehot_encoded)
+
+
+#%% =================================== Normalize Input data =================================== %%#
+# ------------------------ Normalize input features based on thresholds set manually ----------------------------------#
+
+# min and max values for each feature
+#pos_x = (-9, 11)  # ideal +-28
+pos_x = (-16,11)
+pos_y = (-8, 20)  # ideal +-30
+pos_z = (-42, -15)  # ideal +20 +60
+
+vel_x = (0, 65)
+vel_y = (0,70)
+vel_z = (0,90)
+
+ang_vel_x = (0,150)
+ang_vel_y = (0,150)
+ang_vel_z = (0,190)
+
+#  input msx and min to normalize data
+def normalize_dataframe_column_custom(min, max, input_df, feature_list):
+    df_copy = input_df.copy()
+    for feature in feature_list:
+        df_copy[feature] = df_copy[feature].map(lambda a: normalize_input(a, min, max))
+    return df_copy
+
+
+def normalize_array_columns_custom(input_array, thresh_min: int, thresh_max: int):
+    local_array = np.empty([input_array.size])
+    # print('Size of array: ', input_array.size)
+    for x in range(input_array.size):
+        local_array[x] = normalize_input(input_array[x], thresh_min, thresh_max)
+    return local_array
+
+
+# def normalize_dataframe_custom((maxX,minX),(maxY,minY),(maxZ,minZ),(maxA,minA),(maxB,minB),(maxG,minG),input_df):
+def normalize_input(x, x_min, x_max):
+    if x >= x_max:
+        return 1
+    elif x <= x_min:
+        return 0
+    else:
+        return (x - x_min) / (x_max - x_min)
+
+
+def scale_input(value, scale_value):
+    if value > 0:
+        return scale_value - value
+    else:
+        return scale_value + value
+
+
+# use max and min values in sequence to normalize data
+def normalize_column(input_df, feature_name):
+    df_copy = input_df.copy()
+    for feature in feature_name:
+        max_value = input_df[feature].max()
+        min_value = input_df[feature].min()
+        if max_value == min_value:
+            print("Error: Cannot normalize when max and min values are equal")
+            return df_copy
+        df_copy[feature] = (input_df[feature] - min_value) / (max_value - min_value)
+    return df_copy
+
+for file_no in range(len(df)):
+    # normalize position and orientation values
+    df[file_no] = normalize_dataframe_column_custom(pos_x[0], pos_x[1], df, ['X'])
+    df[file_no] = normalize_dataframe_column_custom(pos_y[0], pos_y[1], df, ['Y'])
+    df[file_no] = normalize_dataframe_column_custom(pos_z[0], pos_z[1], df, ['Z'])
+
+    # normalize linear velocity
+    x_norm_linear_velocity = normalize_array_columns_custom(x_linear_velocity, vel_x[0], vel_x[1])
+    y_norm_linear_velocity = normalize_array_columns_custom(y_linear_velocity, vel_y[0], vel_y[1])
+    z_norm_linear_velocity = normalize_array_columns_custom(z_linear_velocity, vel_z[0], vel_z[1])
